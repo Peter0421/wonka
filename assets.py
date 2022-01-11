9f5482ca9da40dd6492771aec62653f346b24070
Peter0421 <32634037+Peter0421@users.noreply.github.com>
2022-01-11 10:58:12 +0100

    Update assets.py

--- assets.py
+++ assets.py
@@ -1,5 +1,9 @@
 import requests
 
-r= requests.get ("https://api.opensea.io/api/v1/assets")
+params = {
+  'collection': 'mutant-ape-yacht-club'
+}
+
+r= requests.get ("https://api.opensea.io/api/v1/assets", params=params)
 
 print(r.json())
